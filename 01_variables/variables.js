5c0ec9919f65794e4e586ffc6b39574c4ea1f736
Amit-Patidar13 <amitpatidar@gmail.com>
2025-04-17 13:19:30 +0530

    first commit

---
+++ 01_variables/variables.js
@@ -0,0 +1,12 @@
+console.log("Amit patidar")
+const accountId = 123456
+let accountEmail = "amitpatidar1310@gmail.com"
+var accountPass = "123456"
+accountPass = "456789"
+// accountId = 789456
+console.log([accountEmail, accountPass]);
+console.log(accountPass);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
